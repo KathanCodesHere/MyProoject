22127d323824ebb0b2fca5bd6e0b5faefc797abb
Sanjna Sisodiya <sanjanasisodiya782@gmail.com>
2025-06-06 04:17:55 +0530

    Sanjna Sisodiya

---
+++ Day1/src/cloning.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <h2>Cloning a repository</h2>
+      <p>
+        When you create a repository on GitHub, it exists as a remote
+        repository. You can clone your repository to create a local copy on your
+        computer and sync between the two locations.
+      </p>
+    </div>
+  );
+};
+
+export default about;
